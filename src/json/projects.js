5ce6564623efa2165b86a1238f024cc657b04701
Ritik Sethi <58940080+ritiksethi21@users.noreply.github.com>
2021-06-14 23:06:59 +0530

    Create projects.js

---
+++ src/json/projects.js
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    img: "https://i.ibb.co/whFkqC6/swag.png",
+    name: "Swag-Store",
+    // ext: "jsx",
+    lang: "js",
+    git: "https://github.com/ritiksethi21/Swag-Store",
+    live: "https://glacial-coast-74170.herokuapp.com/",
+  },
+  // https://bubble-sort-sketch.netlify.app/
+  {
+    img: "https://i.ibb.co/2K2mwNp/videoX.png",
+    name: "VideoX",
+    // ext: "vue",
+    lang: "js",
+    git: "https://github.com/ritiksethi21/VideoX",
+    live: "https://github.com/ritiksethi21/VideoX",
+  },
+  {
+    img: "https://i.ibb.co/kq8ZYVG/insta.png",
+    name: "Instagram Clone",
+    lang: "js",
+    git: "https://github.com/ritiksethi21/Instagram-Clone",
+    live: "https://instagram-clone-2112.web.app/",
+  },
+
+  {
+    img: "https://i.ibb.co/rHFQzRk/sort.png",
+    name: "Bubble Sort",
+    ext: "js",
+    lang: "js",
+    git: "https://github.com/ritiksethi21/sketch/tree/main/BubbleSort",
+    live: "https://bubble-sort-sketch.netlify.app/",
+  },
+  {
+    img: "https://i.ibb.co/Z1wtRt9/sun.png",
+    name: "Ray Casting",
+    ext: "js",
+    lang: "js",
+    git: "https://github.com/ritiksethi21/sketch/tree/main/sun",
+    live: "https://sun-sketch.netlify.app/",
+  },
+];
